576f37da8c38279d8fc81b9f5128a37bf39d802c
dgroumpos <dngroumpos@hotmail.com>
2025-02-16 15:31:52 +0200

    presentation layer initial implementation

--- Grocerie.API/Program.cs
+++ Grocerie.API/Program.cs
@@ -1,17 +1,22 @@
+using Grocerie.Application.Services;
+using Grocerie.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<GroceryListService>();
+builder.Services.AddScoped<GroceryItemService>();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
