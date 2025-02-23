5d2e23c0a384ffb6e150bbc9801faedc006c5f39
dgroumpos <dngroumpos@hotmail.com>
2025-02-23 15:07:15 +0200

    modified GroceryListController to use MediatR

--- Grocerie.API/Controllers/GroceryListController.cs
+++ Grocerie.API/Controllers/GroceryListController.cs
@@ -1,5 +1,6 @@
-using Grocerie.Application.Services;
-using Grocerie.Domain.Entities;
+using Grocerie.Application.Commands.GroceryListCommands;
+using Grocerie.Application.Queries.GroceryListQueries;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,43 @@ namespace Grocerie.API.Controllers;
 [Authorize]
 [Route("api/[controller]")]
 [ApiController]
-public class GroceryListController(GroceryListService service) : ControllerBase
+public class GroceryListController(IMediator mediator) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAllLists() => Ok(await service.GetAllListsAsync());
+    public async Task<IActionResult> GetAllLists()
+    {
+        var lists = await mediator.Send(new GetAllGroceryListsQuery());
+        return Ok(lists);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetListById(GetGroceryListByIdQuery query)
+    {
+        var list = await mediator.Send(query);
+        return list is not null ? Ok(list) : NotFound();
+    }
 
     [HttpPost]
-    public async Task<IActionResult> AddList([FromBody] GroceryList list)
+    public async Task<IActionResult> AddList(CreateGroceryListCommand command)
     {
-        await service.AddListAsync(list);
-        return Ok("List added successfully");
+      var id = await mediator.Send(command);
+      return Created(nameof(GetListById), id);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateList(Guid id, [FromBody]UpdateGroceryListCommand command)
+    {
+        if(id != command.Id)
+            return BadRequest("Ids do not match");
+        
+        var success = await mediator.Send(command);
+        return success ? Ok("List updated successfully") : NotFound("List not found");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteList(Guid id)
+    {
+        var success = await mediator.Send(new DeleteGroceryListCommand(id));
+        return success ? Ok("List deleted successfully") : NotFound("List not found");
     }
 }
