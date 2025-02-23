e87d7f9713ebd148f6b9a159ebafac07a1df627b
dgroumpos <dngroumpos@hotmail.com>
2025-02-23 17:14:57 +0200

    updated ApplicationDbContext and entities, created migrations

--- Grocerie.Application/Handlers/GroceryListHandlers/Commands/CreateGroceryListHandler.cs
+++ Grocerie.Application/Handlers/GroceryListHandlers/Commands/CreateGroceryListHandler.cs
@@ -9,7 +9,7 @@ public class CreateGroceryListHandler(IGenericRepository<GroceryList> repository
 {
     public async Task<Guid> Handle(CreateGroceryListCommand request, CancellationToken cancellationToken)
     {
-        var list = new GroceryList {Id = Guid.NewGuid(), Title = request.Title, UserId = request.UserId};
+        var list = new GroceryList {Id = Guid.NewGuid(), Title = request.Title, UserId = request.UserId.ToString()};
         await repository.AddAsync(list);
         await repository.SaveChangesAsync();
         return list.Id;
--- Grocerie.Domain/Entities/GroceryList.cs
+++ Grocerie.Domain/Entities/GroceryList.cs
@@ -8,6 +8,6 @@ public class GroceryList
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public List<GroceryItem> Items { get; set; } = [];
-    public Guid UserId { get; set; } //FK
+    public string UserId { get; set; } //FK
     public User User { get; set; } //Navigation property
 }
